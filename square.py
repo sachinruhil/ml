6c783f9cfafee9755d96a78a619c89d578eb6ec5
Sachin Ruhil <sachinruhil@Sachins-MacBook-Pro.local>
2018-02-27 13:42:02 +0530

    function with multiple returns

--- square.py
+++ square.py
@@ -29,7 +29,19 @@ def which_prize(x):
         return ("Congratulations! you have won a wafer-thin mint")
     elif x in range(181, 201):
         return ("Congratulations! you have won a penguin")
-
-
 message = which_prize(55)
 print(message)
+
+
+# another if with return
+
+def cylinder_surface_area(radius, height, has_top_and_bottom):
+    side_area = height * 6.28 * radius
+    if has_top_and_bottom:
+        top_area = 3.14 * radius ** 2
+        return  2 * top_area + side_area
+    else:
+        return side_area
+call = cylinder_surface_area(2,4,True)
+print(call)
+
