61d78d2eacdd753e4b60c13fca3d3497949be4d1
Sachin Ruhil <sachinruhil@Sachins-MacBook-Pro.local>
2018-02-28 23:27:53 +0530

    slicing lists

--- square.py
+++ square.py
@@ -113,3 +113,19 @@ def scores_to_rating(score1, score2, score3, score4, score5):
 call = scores_to_rating(1, 3, 5, 7, 9)
 print(call)
 
+
+
+#slicing list
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+
+# TODO: Modify this line so it prints the last three elements of the list
+print(eclipse_dates)
+
+first_half = eclipse_dates[:5]
+print(first_half)
+
+second_half = eclipse_dates[5:]
+print(second_half)
